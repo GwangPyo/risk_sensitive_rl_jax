f6153e2b042e14f6914c9b46db749d5387f5aea0
yoo <necrocathy@gmail.com>
2023-03-14 01:58:32 +0900

    refactor typos, fix sum options

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(
     author='Gwangpyo Yoo',
     author_email='necrocathy@gmail.com',
     requires=[],
-    install_requires=['wandb', 'dm-haiku', 'optax', 'gym==0.21', ],
+    install_requires=['wandb', 'dm-haiku', 'optax', 'gym==0.21', 'fire'],
     packages=find_packages(exclude=[])
 )
